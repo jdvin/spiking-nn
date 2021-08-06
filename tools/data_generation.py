fd750ef3afd983ed093ff02117b1c7a41e1fc51d
Josh <josh.daniel.vinson@gmail.com>
2021-08-06 23:34:53 +1000

    removed hideous probabilistic running time funcs

--- tools/data_generation.py
+++ tools/data_generation.py
@@ -3,40 +3,43 @@ import random as rand
 def populate_vector(vector, n):
     '''
     turn n zeros in vector into ones
-    NB: THIS WILL RUN FOREVER IF THERE ARE NOT at least n zeroes 
     '''
-    for _ in range(n):
-        found = False
-        while not found:
-            i = rand.randint(0, len(vector)-1)
-            if not vector[i]:
-                vector[i] = 1
-                found = True
+    population = [i for i,element in enumerate(vector) if element == 0]
+    sample = rand.sample(population, n)
+    
+    for i in sample:
+        vector[i] = 1
+
     return vector
 
 def unpopulate_vector(vector, n):
     '''
     turn n ones in vector into zeros
-    NB: THIS WILL RUN FOREVER IF THERE ARE NOT at least n ones 
     '''
-    for _ in range(n):
-        found = False
-        while not found:
-            i = rand.randint(0, len(vector)-1)
-            if vector[i]:
-                vector[i] = 0
-                found = True
-    return vector
+    population = [i for i,element in enumerate(vector) if element == 1]
+    sample = rand.sample(population, n)
+    
+    for i in sample:
+        vector[i] = 0
 
+    return vector
+ 
 def generate_data(n, len_, sparcity, overlap):
+    '''
+    (sparcity - overlap) * n must be less than overlap
+    '''
     data = []
     prototype = [0 for _ in range(len_)]
     prototype_sparticity = int(len_ * overlap)
     prototype = populate_vector(prototype, prototype_sparticity)
 
+    prim_pool = [element for i,element in enumerate(prototype) if element == 0]
+    all_exem_prims = rand.sample(prim_pool,  int((len_* (sparcity-overlap) * n)))
+
     for _ in range(n):
         exemplar = prototype.copy()
-        exemplar_sparcity = int(len_ * (sparcity - overlap))
-        exemplar = populate_vector(exemplar, exemplar_sparcity)
+        exemplar_prims = rand.sample(all_exem_prims, int(len(all_exem_prims)/n))
+        for i in exemplar_prims:
+            exemplar[i] = 1
         data.append(exemplar.copy())
     return data
